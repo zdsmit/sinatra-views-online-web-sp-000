1a9c28cc9d63a77887d909db3e86b6105701bab3
Zach  Smith <zdsmith001@gmail.com>
2019-11-12 23:34:47 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,7 +2,10 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-	get '/' do
-	 erb :index
+  get '/' do
+	  erb :index
 	end
-end
+
+  get "/info" do
+     "Testing the info page"
+  end
